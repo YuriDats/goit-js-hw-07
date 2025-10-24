3c8ab3854617b35eb1f1dca5366347a909948e77
YuriDats <datsiuk.ukr@gmail.com>
2025-10-25 02:13:18 +0300

    fix2

--- js/task-4.js
+++ js/task-4.js
@@ -3,24 +3,35 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", Foo);
 
 function Foo(event) {
-    event.preventDefault();
-    const elements = event.target.elements;
+     event.preventDefault();
+    // const elements = event.target.elements;
 
 
-    const info = {
-        email: elements.email.value,
-        password: elements.password.value
-    }
+    // const info = {
+    //     email: elements.email.value,
+    //     password: elements.password.value
+    // }
 
     
-    if(elements.email.value.trim() === "" || elements.password.value.trim() === ""){
-        alert('All form fields must be filled in');
+    // if(elements.email.value.trim() === "" || elements.password.value.trim() === ""){
+    //     alert('All form fields must be filled in');
+    // }
+    // else {
+    //     console.log(`Email: ${info.email}, Password: ${info.password}`);
+    // }
+    const elements = event.target.elements;
+
+    const email = elements.email.value.trim();
+    const password = elements.password.value.trim();
+
+    if (email === "" || password === "") {
+        alert("All form fields must be filled in");
+        return;
     }
-    else {
-        console.log(`Email: ${info.email}, Password: ${info.password}`);
-    }
 
-
-
+    const info = { email, password };
+    console.log(info);
     event.target.reset(); 
 }
+
+
